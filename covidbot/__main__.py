aad52be727b5cdecb5b85e908b4ab4c8cd09f7a5
Soenke Huster <me@eknoes.de>
2021-01-04 11:50:07 +0100

    Add --message flag to sent corrected report
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/__main__.py
+++ covidbot/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 from covidbot.bot import Bot
 from covidbot.covid_data import CovidData
@@ -10,6 +11,12 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # Also write to stderr
 logging.getLogger().addHandler(logging.StreamHandler())
 
+# Add arguments
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--message', help='Do not start the bot but send a message to all users', action='store_true')
+args = parser.parse_args()
+
 # Initialize Data
 with open(".api_key", "r") as f:
     key = f.readline().rstrip("\n")
@@ -18,4 +25,24 @@ with open(".db_password", "r") as f:
     password = f.readline().rstrip("\n")
 data = CovidData(db_user="covid_bot", db_password=password, db_name="covid_bot_db")
 bot = TelegramInterface(Bot(data, SubscriptionManager("user.json")), api_key=key)
-bot.run()
+
+if args.message:
+    print()
+    if input("If you want to sent a correction message with the current report to all users, press Y: ") != "Y":
+        exit(0)
+
+    line = input("Message (Basic HTML allowed):\n")
+    lines = []
+    while True:
+        if line:
+            lines.append(line)
+        else:
+            break
+        line = input()
+    msg = '\n'.join(lines)
+
+    print(f"\n\n{msg}\n\n")
+    if input("Press Y to send this message: ") == "Y":
+        bot.send_correction_message(msg)
+else:
+    bot.run()
