5c5de5ba26f58d709531527a6431a10c69c55c24
Soenke Huster <me@eknoes.de>
2021-01-28 08:45:09 +0100

    Add basic Threema Interface
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/threema_interface.py
+++ covidbot/threema_interface.py
@@ -1,8 +1,11 @@
 import logging
+import os
+from io import BytesIO
+from typing import Dict
 
 import threema.gateway as threema
 from aiohttp import web
-from threema.gateway.e2e import create_application, add_callback_route
+from threema.gateway.e2e import create_application, add_callback_route, TextMessage, Message, ImageMessage
 
 from covidbot.bot import Bot
 from covidbot.text_interface import SimpleTextInterface
@@ -25,6 +28,10 @@ class ThreemaInterface(SimpleTextInterface):
             secret=self.threema_secret,
             key=self.threema_key
         )
+        self.graphics_tmp_path = os.path.abspath("tmp-threema/")
+        if not os.path.isdir(self.graphics_tmp_path):
+            os.makedirs(self.graphics_tmp_path)
+
 
     def run(self):
         logging.info("Run Threema Interface")
@@ -33,5 +40,20 @@ class ThreemaInterface(SimpleTextInterface):
         add_callback_route(self.connection, application, self.handle_threema_msg, path='/gateway_callback')
         web.run_app(application, port=9000)
 
-    async def handle_threema_msg(self, message: str):
-        print('Got message ({}): {}'.format(repr(message), message))
+    def get_attachment(self, image: BytesIO) -> Dict:
+        filename = self.graphics_tmp_path + "/graphic.jpg"
+        with open(filename, "wb") as f:
+            image.seek(0)
+            f.write(image.getbuffer())
+        return {"filename": filename, "width": "900", "height": "600"}
+
+    async def handle_threema_msg(self, message: Message):
+        if type(message) == TextMessage:
+            message: TextMessage
+            response = self.handle_input(message.text, message.from_id)
+            if response.image:
+                response_img = ImageMessage(self.connection, image=self.get_attachment(response.image)['filename'])
+                await response_img.send()
+
+            response_msg = TextMessage(self.connection, text=response.message, to_id=message.from_id)
+            await response_msg.send()
