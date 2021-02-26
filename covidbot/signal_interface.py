3cf5b8d0e6b2cdf68fd729290d4af62510db405c
Soenke Huster <me@eknoes.de>
2021-02-26 23:19:33 +0100

    Improve SignalInterface, add documentation strings
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/signal_interface.py
+++ covidbot/signal_interface.py
@@ -37,26 +37,29 @@ class SignalInterface(SimpleTextInterface, MessengerInterface):
             os.makedirs(self.graphics_tmp_path)
 
     def run(self):
-        asyncio.run(self._run())
+        asyncio.run(self.run_async())
 
-    async def _run(self):
+    async def run_async(self):
         async with semaphore.Bot(self.phone_number, socket_path=self.socket, profile_name=self.profile_name,
                                  profile_picture=self.profile_picture) as bot:
-            bot.register_handler(re.compile(""), self.text_handler)
-            bot.set_exception_handler(self.exception_handler)
+            # We do not really use the underlying bot framework, but just use our own Pure-Text Handler
+            bot.register_handler(re.compile(""), self.message_handler)
+            bot.set_exception_handler(self.exception_callback)
             await bot.start()
 
-    async def exception_handler(self, exception: Exception, ctx: ChatContext):
+    async def exception_callback(self, exception: Exception, ctx: ChatContext):
         self.log.exception("An exception occurred, exiting...", exc_info=exception)
         tb_list = traceback.format_exception(None, exception, exception.__traceback__)
         tb_string = ''.join(tb_list)
 
-        await self.sendMessageToDev(f"Exception occurred: {tb_string}\n\n"
-                                    f"Got message {ctx.message}", ctx.bot)
+        await self.send_to_dev(f"Exception occurred: {tb_string}\n\nGot message {ctx.message}", ctx.bot)
         # Just exit on exception
         os.kill(os.getpid(), signal.SIGINT)
 
-    async def text_handler(self, ctx: ChatContext):
+    async def message_handler(self, ctx: ChatContext):
+        """
+        Handles a text message received by the bot
+        """
         text = ctx.message.get_body()
         if text:
             await ctx.message.typing_started()
@@ -72,19 +75,31 @@ class SignalInterface(SimpleTextInterface, MessengerInterface):
                 self.bot.enable_user(platform_id)
             reply = self.handle_input(text, platform_id)
             if reply:
-                await self.reply_message(ctx, reply)
+                await self.send_reply(ctx, reply)
             await ctx.message.typing_stopped()
 
-    async def reply_message(self, ctx: ChatContext, reply: BotResponse):
+    async def send_reply(self, ctx: ChatContext, reply: BotResponse):
+        """
+        Answers a signal message with the given :class:`covidbot.BotResponse`
+        """
         reply.message = adapt_text(reply.message)
 
         attachment = []
         if reply.image:
-            attachment.append(self.get_attachment(reply.image))
+            attachment.append(self.get_attachment_path(reply.image))
 
         await ctx.message.reply(body=reply.message, attachments=attachment)
 
-    def get_attachment(self, image: BytesIO, district_id=99) -> Dict:
+    def get_attachment_path(self, image: BytesIO, district_id=99) -> Dict:
+        """
+        Returns an attachement dict to send an image with signald, containing a file path to the graphic
+        Args:
+            image: Image
+            district_id: ID which should be used for caching
+
+        Returns:
+
+        """
         filename = self.graphics_tmp_path + f"/graphic{district_id}.jpg"
         with open(filename, "wb") as f:
             image.seek(0)
@@ -92,14 +107,17 @@ class SignalInterface(SimpleTextInterface, MessengerInterface):
         return {"filename": filename, "width": "900", "height": "600"}
 
     async def send_daily_reports(self) -> None:
+        """
+        Send unconfirmed daily reports to the specific users
+        """
+        # Get reports
         unconfirmed_reports = self.bot.get_unconfirmed_daily_reports()
         if not unconfirmed_reports:
             return
-        self.log.warning(f"{len(unconfirmed_reports)} to send!")
-        attachment = self.get_attachment(self.bot.get_graphical_report(0), 0)
 
-        # This way, if we are blocked from a single user or such, we should reach all other users
-        random.shuffle(unconfirmed_reports)
+        # Get the current graph as attachement dict
+        self.log.warning(f"{len(unconfirmed_reports)} to send!")
+        country_graph = self.get_attachment_path(self.bot.get_graphical_report(0), 0)
 
         async with semaphore.Bot(self.phone_number, socket_path=self.socket, profile_name=self.profile_name,
                                  profile_picture=self.profile_picture) as bot:
@@ -107,28 +125,80 @@ class SignalInterface(SimpleTextInterface, MessengerInterface):
             message_counter = 0
             for userid, message in unconfirmed_reports:
                 self.log.info(f"Try to send report {message_counter}")
-
-                success = await bot.send_message(userid, adapt_text(message), attachments=[attachment])
+                success = await bot.send_message(userid, adapt_text(message), attachments=[country_graph])
                 if success:
                     self.bot.confirm_daily_report_send(userid)
                     self.log.warning(f"({message_counter}/{len(unconfirmed_reports)}) Sent daily report to {userid}")
-                    if backoff_time > 1:
-                        backoff_time = 0.7 * backoff_time
                 else:
                     self.log.error(
                         f"({message_counter}/{len(unconfirmed_reports)}) Error sending daily report to {userid}")
-                    backoff_time = 2 ^ ceil(backoff_time)
-                    # Disable user, hacky workaround for https://github.com/eknoes/covidbot/issues/103
-                    self.bot.disable_user(userid)
 
-                sleep_seconds = backoff_time
-                self.log.info(f"Sleeping {sleep_seconds}s to avoid server limitations")
-                time.sleep(sleep_seconds)
+                backoff_time = self.backoff_timer(backoff_time, not success, userid)
                 message_counter += 1
 
         await self.restart_service()
 
-    async def restart_service(self):
+    async def send_message(self, message: str, users: List[str], append_report=False) -> None:
+        """
+        Send a message to specific or all users
+        Args:
+            message: Message to send
+            users: List of user ids or None for all signal users
+            append_report: True if a current report should be appended
+        """
+        if not users:
+            users = map(lambda x: x.platform_id, self.bot.get_all_user())
+
+        async with semaphore.Bot(self.phone_number, socket_path=self.socket, profile_name=self.profile_name,
+                                 profile_picture=self.profile_picture) as bot:
+            backoff_time = random.uniform(0.5, 2)
+            for user in users:
+                success = await bot.send_message(user, adapt_text(message))
+                backoff_time = self.backoff_timer(backoff_time, not success, user)
+
+                if append_report:
+                    response = self.reportHandler("", user)
+                    attachments = []
+                    if response.image:
+                        attachments.append(self.get_attachment_path(response.image))
+                    success = await bot.send_message(user, adapt_text(response.message), attachments)
+                    backoff_time = self.backoff_timer(backoff_time, not success, user)
+
+        await self.restart_service()
+
+    def backoff_timer(self, current_backoff: float, failed: bool, user_id: str) -> float:
+        """
+        Sleeps and calculates the new backoff time, depending whether sending the message failed or not
+        Args:
+            current_backoff: current backoff time in seconds
+            failed: True if sending the message led to an error
+            user_id: ID of the receiver
+
+        Returns:
+            float: new backoff time
+        """
+        if not failed:
+            self.log.info(f"Sent message to {user_id}")
+            if current_backoff > 1:
+                new_backoff = 0.7 * current_backoff
+            else:
+                new_backoff = current_backoff
+        else:
+            self.log.error(f"Error sending message to {user_id}")
+            # Disable user, hacky workaround for https://github.com/eknoes/covidbot/issues/103
+            self.bot.disable_user(user_id)
+            new_backoff = 2 ^ ceil(current_backoff)
+        self.log.info(f"Sleeping {new_backoff}s to avoid server limitations")
+        time.sleep(new_backoff)
+        return new_backoff
+
+    async def send_to_dev(self, message: str, bot: semaphore.Bot):
+        await bot.send_message(self.dev_chat, adapt_text(message))
+
+    async def restart_service(self) -> None:
+        """
+        Restarts the signald and signalbot service
+        """
         self.log.warning("Try to restart signald and signalbot")
         cmd = "supervisorctl restart signald signalbot"
         proc = await asyncio.create_subprocess_shell(
@@ -142,55 +212,3 @@ class SignalInterface(SimpleTextInterface, MessengerInterface):
             self.log.error(f'{cmd!r} exited with {proc.returncode}')
             return
         self.log.warning("Restarted signalbot service")
-
-    async def send_message(self, message: str, users: List[str], append_report=False):
-        if not users:
-            users = map(lambda x: x.platform_id, self.bot.get_all_user())
-
-        async with semaphore.Bot(self.phone_number, socket_path=self.socket, profile_name=self.profile_name,
-                                 profile_picture=self.profile_picture) as bot:
-            backoff_time = random.uniform(0.5, 2)
-            message_counter = 0
-            for user in users:
-                success = await bot.send_message(user, adapt_text(message))
-                if success:
-                    self.log.warning(f"Sent message to {user}")
-                    if backoff_time > 1:
-                        backoff_time = 0.7 * backoff_time
-                else:
-                    self.log.error(f"Error sending message to {user}")
-                    backoff_time = 2 ^ ceil(backoff_time)
-                    # Disable user, hacky workaround for https://github.com/eknoes/covidbot/issues/103
-                    self.bot.disable_user(user)
-
-                sleep_seconds = backoff_time
-                self.log.info(f"Sleeping {sleep_seconds}s to avoid server limitations")
-                time.sleep(sleep_seconds)
-                message_counter += 1
-
-                if append_report:
-                    response = self.reportHandler("", user)
-                    attachments = []
-                    if response.image:
-                        attachments.append(self.get_attachment(response.image))
-                    success = await bot.send_message(user, adapt_text(response.message), attachments)
-
-                    if success:
-                        self.log.warning(f"Sent message to {user}")
-                        if backoff_time > 1:
-                            backoff_time = 0.7 * backoff_time
-                    else:
-                        self.log.error(f"Error sending message to {user}")
-                        backoff_time = 2 ^ ceil(backoff_time)
-                        # Disable user, hacky workaround for https://github.com/eknoes/covidbot/issues/103
-                        self.bot.disable_user(user)
-
-                    sleep_seconds = backoff_time
-                    self.log.info(f"Sleeping {sleep_seconds}s to avoid server limitations")
-                    time.sleep(sleep_seconds)
-                    message_counter += 1
-
-        await self.restart_service()
-
-    async def sendMessageToDev(self, message: str, bot: semaphore.Bot):
-        await bot.send_message(self.dev_chat, adapt_text(message))
