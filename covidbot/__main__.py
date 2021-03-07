0b863b5c70f69e64aaa48f13d5efb2add69d5e9a
Soenke Huster <me@eknoes.de>
2021-03-07 19:50:37 +0100

    Add parameter to Threema
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/__main__.py
+++ covidbot/__main__.py
@@ -86,12 +86,12 @@ class MessengerBotSetup:
         if self.name == "threema":
             return ThreemaInterface(self.config['THREEMA'].get('ID'), self.config['THREEMA'].get('SECRET'),
                                     self.config['THREEMA'].get('PRIVATE_KEY'), bot,
-                                    dev_chat=self.config['THREEMA'].get('DEV_CHAT'))
+                                    dev_chat=self.config['THREEMA'].get('DEV_CHAT'), data_visualization=visualization)
 
         if self.name == "signal":
             return SignalInterface(self.config['SIGNAL'].get('PHONE_NUMBER'),
                                    self.config['SIGNAL'].get('SIGNALD_SOCKET'), bot,
-                                   dev_chat=self.config['SIGNAL'].get('DEV_CHAT'),  data_visualization=visualization)
+                                   dev_chat=self.config['SIGNAL'].get('DEV_CHAT'), data_visualization=visualization)
 
         if self.name == "telegram":
             return TelegramInterface(bot, api_key=self.config['TELEGRAM'].get('API_KEY'),
