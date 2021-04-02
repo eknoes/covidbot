a0d8cd1712840794aadd431d7bb63852e4bee542
errotu <errotu@users.noreply.github.com>
2021-04-02 17:47:35 +0200

    Bugfix

--- covidbot/bot.py
+++ covidbot/bot.py
@@ -286,7 +286,7 @@ class Bot(object):
                        f"In {format_noun(current_data.icu_data.occupied_covid, FormattableNoun.BEDS)} " \
                        f"({format_float(current_data.icu_data.percent_covid())}%) liegen Patient:innen" \
                        f" mit COVID-19, davon müssen {format_noun(current_data.icu_data.covid_ventilated, FormattableNoun.PERSONS)}" \
-                       f" ({format_float(country.icu_data.percent_ventilated())}%) invasiv beatmet werden. " \
+                       f" ({format_float(current_data.icu_data.percent_ventilated())}%) invasiv beatmet werden. " \
                        f"Insgesamt gibt es {format_noun(current_data.icu_data.total_beds(), FormattableNoun.BEDS)}.\n\n"
             sources.append(f'Intensivbettenauslastung vom {current_data.icu_data.date.strftime("%d.%m.%Y")}. '
                            f'Daten vom <a href="https://intensivregister.de">DIVI-Intensivregister</a>.')
