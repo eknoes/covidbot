af7e8d40cc55738b11ae0891398dcea8f6037fe1
Soenke Huster <me@eknoes.de>
2021-04-03 18:21:03 +0200

    Do not send message on new key
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- resources/signal-trust-new-keys.sh
+++ resources/signal-trust-new-keys.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# SET SIGNAL_ADMIN_PHONE as environment variable
-
 signald() {
             echo "$1" | nc -q0 -U ~/covid-bot/resources/signald.sock | jq 'select(.type != "version")'
 }
@@ -10,6 +8,5 @@ signald() {
 signald '{"type": "list_accounts"}' | jq -r 'select(.type == "account_list") | .data.accounts[].username' | while read username; do
         signald "{\"type\": \"get_identities\", \"username\": \"$username\"}" | jq -rc ".data.identities[] | select(.trust_level == \"UNTRUSTED\") | \"{\\\"type\\\": \\\"trust\\\", \\\"fingerprint\\\": \\\"\(.fingerprint)\\\", \\\"recipientAddress\\\": \(.address), \\\"username\\\": \\\"$username\\\"}\"" | while read trustcmd; do
                 signald "$trustcmd"
-                signald "{\"type\": \"send\", \"recipientAddress\": \"{\"phone\": \"$SIGNAL_ADMIN_PHONE\"}\", \"username\": \"$username\", \"messageBody\": \"$(echo $trustcmd | jq -r .recipientAddress.number) ($(echo $trustcmd | jq -r .recipientAddress.uuid)) new fingerprint is $(echo $trustcmd | jq -r .fingerprint)\"}"
         done
 done
