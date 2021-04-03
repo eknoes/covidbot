39403d0764ef113047bd0d3913e326631a36c70f
Soenke Huster <me@eknoes.de>
2021-04-03 18:12:06 +0200

    Add script to trust new signal keys
    Signed-off-by: Soenke Huster <me@eknoes.de>

---
+++ resources/signal-trust-new-keys.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# SET SIGNAL_ADMIN_PHONE as environment variable
+
+signald() {
+            echo "$1" | nc -q0 -U signald.sock | jq 'select(.type != "version")'
+}
+
+signald '{"type": "list_accounts"}' | jq -r 'select(.type == "account_list") | .data.accounts[].username' | while read username; do
+        signald "{\"type\": \"get_identities\", \"username\": \"$username\"}" | jq -rc ".data.identities[] | select(.trust_level == \"UNTRUSTED\") | \"{\\\"type\\\": \\\"trust\\\", \\\"fingerprint\\\": \\\"\(.fingerprint)\\\", \\\"recipientAddress\\\": \(.address), \\\"username\\\": \\\"$username\\\"}\"" | while read trustcmd; do
+                signald "$trustcmd"
+                signald "{\"type\": \"send\", \"recipientAddress\": \"{\"phone\": \"$SIGNAL_ADMIN_PHONE\"}\", \"username\": \"$username\", \"messageBody\": \"$(echo $trustcmd | jq -r .recipientAddress.number) ($(echo $trustcmd | jq -r .recipientAddress.uuid)) new fingerprint is $(echo $trustcmd | jq -r .fingerprint)\"}"
+        done
+done
