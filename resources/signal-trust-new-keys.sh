67d7f5f7091ec3f17789aefcb15f2a0af26cf677
Soenke Huster <me@eknoes.de>
2021-04-03 18:29:35 +0200

    Fix sending message
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- resources/signal-trust-new-keys.sh
+++ resources/signal-trust-new-keys.sh
@@ -10,6 +10,6 @@ signald() {
 signald '{"type": "list_accounts"}' | jq -r 'select(.type == "account_list") | .data.accounts[].username' | while read username; do
         signald "{\"type\": \"get_identities\", \"username\": \"$username\"}" | jq -rc ".data.identities[] | select(.trust_level == \"UNTRUSTED\") | \"{\\\"type\\\": \\\"trust\\\", \\\"fingerprint\\\": \\\"\(.fingerprint)\\\", \\\"recipientAddress\\\": \(.address), \\\"username\\\": \\\"$username\\\"}\"" | while read trustcmd; do
                 signald "$trustcmd"
-                signald "{\"type\": \"send\", \"version\": \"v1\", \"recipientAddress\": {\"phone\": \"$SIGNAL_ADMIN_PHONE\"}, \"username\": \"$username\", \"messageBody\": \"$(echo $trustcmd | jq -r .recipientAddress.number) ($(echo $trustcmd | jq -r .recipientAddress.uuid)) new fingerprint is $(echo $trustcmd | jq -r .fingerprint)\"}"
+                signald "{\"type\": \"send\", \"version\": \"v1\", \"recipientAddress\": {\"number\": \"$SIGNAL_ADMIN_PHONE\"}, \"username\": \"$username\", \"messageBody\": \"$(echo $trustcmd | jq -r .recipientAddress.number) ($(echo $trustcmd | jq -r .recipientAddress.uuid)) new fingerprint is $(echo $trustcmd | jq -r .fingerprint)\"}"
         done
 done
