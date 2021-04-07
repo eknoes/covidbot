18c5ba342339612bd9539be9ffaee77ae2cfaed2
Soenke Huster <me@eknoes.de>
2021-04-07 21:14:59 +0200

    Fix geolocation bug
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/bot.py
+++ covidbot/bot.py
@@ -137,7 +137,7 @@ class Bot(object):
             return BotResponse(message), possible_district
         else:
             message = "Mit deinem Suchbegriff wurden mehr als 15 Orte gefunden, bitte versuche spezifischer zu sein."
-            return [BotResponse(message)], None
+            return BotResponse(message), None
 
     def find_district_id_from_geolocation(self, lon, lat) -> Tuple[Optional[str], Optional[List[District]]]:
         district_id = self._location_service.find_rs(lon, lat)
@@ -152,7 +152,7 @@ class Bot(object):
             message = None
             if district.parent:
                 message = "Die Daten für die folgenden Orte und Regionen sind für deinen Standort verfügbar"
-                results.append((district.parent, self._data.get_district(district.parent).name))
+                results.append(self._data.get_district(district.parent))
             return message, results
 
     def get_possible_actions(self, user_identification: Union[int, str], district_id: int) -> Tuple[
