83a752c9feee122a2d2abdd9f4d6ef5114dabf52
Soenke Huster <me@eknoes.de>
2021-04-27 23:29:51 +0200

    Fix preferences
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/bot.py
+++ covidbot/bot.py
@@ -657,7 +657,7 @@ class Bot(object):
             for setting in [BotUserSettings.BETA, BotUserSettings.REPORT_GRAPHICS, BotUserSettings.FORMATTING,
                             BotUserSettings.REPORT_INCLUDE_ICU, BotUserSettings.REPORT_INCLUDE_VACCINATION,
                             BotUserSettings.REPORT_EXTENSIVE_GRAPHICS]:
-                if BotUserSettings.command_key(setting) != user_input[0]:
+                if BotUserSettings.command_key(setting).lower() != user_input[0].lower():
                     continue
 
                 if len(user_input) >= 2:
@@ -713,59 +713,10 @@ class Bot(object):
             return [BotResponse(message, choices=choices)]
 
     def graphicSettingsHandler(self, user_input: str, user_id: int) -> List[BotResponse]:
-        BOT_COMMAND_COUNT.labels('graphic').inc()
-        if user_input:
-            if user_input[:3] == "ein":
-                self.user_manager.set_user_setting(user_id, BotUserSettings.REPORT_GRAPHICS, True)
-                return [BotResponse("Dein Bericht enthält nun Grafiken.")]
-            elif user_input[:3] == "aus":
-                self.user_manager.set_user_setting(user_id, BotUserSettings.REPORT_GRAPHICS, False)
-                return [BotResponse("Dein Bericht enthält nun keine Grafiken mehr.")]
-
-        current = self.user_manager.get_user_setting(user_id, BotUserSettings.REPORT_GRAPHICS)
-        choices = []
-        state: str
-        if current:
-            state = 'aktiviert'
-            choices.append(UserChoice('Deaktivieren', '/grafik aus', 'Sende \"Grafik aus\", um Grafiken im Bericht '
-                                                                     'zu deaktivieren'))
-        else:
-            choices.append(UserChoice('Aktivieren', '/grafik ein', 'Sende \"Grafik ein\", um Grafiken im Bericht '
-                                                                   'zu aktivieren'))
-            state = 'deaktiviert'
-
-        choices.append(UserChoice("Abbrechen", "/noop", "Sende etwas anderes, um mit der Benutzung wie gewohnt "
-                                                        "fortzufahren"))
-        message = f"Die Verwendung von Grafiken im Bericht ist für dich {state}."
-
-        return [BotResponse(message, choices=choices)]
+        return self.settingsHandler(BotUserSettings.command_key(BotUserSettings.REPORT_GRAPHICS) + ' ' + user_input, user_id)
 
     def betaSettingsHandler(self, user_input: str, user_id: int) -> List[BotResponse]:
-        BOT_COMMAND_COUNT.labels('beta').inc()
-        if user_input:
-            if user_input[:3] == "ein":
-                self.user_manager.set_user_setting(user_id, BotUserSettings.BETA, True)
-                return [BotResponse("Du nimmst nun an der Beta teil.")]
-            elif user_input[:3] == "aus":
-                self.user_manager.set_user_setting(user_id, BotUserSettings.BETA, False)
-                return [BotResponse("Du nimmst nun nicht mehr an der Beta teil.")]
-
-        current = self.user_manager.get_user_setting(user_id, BotUserSettings.BETA)
-        choices = []
-        state: str
-        if current:
-            state = ''
-            choices.append(
-                UserChoice('Deaktivieren', '/beta aus', 'Sende \"Beta aus\", um nicht an der Beta teilzunehmen'))
-        else:
-            choices.append(UserChoice('Aktivieren', '/beta ein', 'Sende \"Beta ein\", um an der Beta teilzunehmen'))
-            state = ' nicht'
-
-        choices.append(UserChoice("Abbrechen", "/noop", "Sende etwas anderes, um mit der Benutzung wie gewohnt "
-                                                        "fortzufahren"))
-        message = f"Du nutzt die Beta-Version{state}. In der Beta Version testen wir aktuell einen verbesserten Bericht."
-
-        return [BotResponse(message, choices=choices)]
+        return self.settingsHandler(BotUserSettings.command_key(BotUserSettings.BETA) + ' ' + user_input, user_id)
 
     def deleteMeHandler(self, user_input: str, user_id: int) -> List[BotResponse]:
         BOT_COMMAND_COUNT.labels('delete_me').inc()
