2e68500d1fa971e2fcc247ae64cfc58fdc504586
Soenke Huster <me@eknoes.de>
2021-05-19 15:55:25 +0200

    Fix Incidence History updater overwriting current incidences
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- covidbot/covid_data/updater/cases.py
+++ covidbot/covid_data/updater/cases.py
@@ -261,6 +261,10 @@ class RKIHistoryUpdater(RKIUpdater):
                         self.log.info(f"Got historic incidence data for {updated}")
                         continue
 
+                    # Do not overwrite current data
+                    if (date.today() - updated).days <= 7:
+                        continue
+
                     district_id = field[:-4]
                     if district_id == '11000':
                         district_id = '11'
