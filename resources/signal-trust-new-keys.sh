a888a739542f1c5c62568b750da97fea42e625b2
Soenke Huster <me@eknoes.de>
2021-09-06 11:56:06 +0200

    Update path to signald socket, see #198
    Signed-off-by: Soenke Huster <me@eknoes.de>

--- resources/signal-trust-new-keys.sh
+++ resources/signal-trust-new-keys.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # SET SIGNAL_ADMIN_PHONE as environment variable
 
 signald() {
-            echo "$1" | nc -U ~/covid-bot/resources/signald.sock | jq 'select(.type != "version")'
+            echo "$1" | nc -U /var/run/signald/signald.sock | jq 'select(.type != "version")'
 }
 
 signald '{"type": "list_accounts"}' | jq -r 'select(.type == "account_list") | .data.accounts[].username' | while read username; do
