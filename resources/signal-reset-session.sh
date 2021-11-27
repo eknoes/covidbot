4fa7c7fdcb2f4057d7507f500096c6f6ce2cf47d
Soenke Huster <soenke.huster@eknoes.de>
2021-11-27 23:08:47 +0100

    Improve signal trust key script
    Signed-off-by: Soenke Huster <soenke.huster@eknoes.de>

--- resources/signal-reset-session.sh
+++ resources/signal-reset-session.sh
@@ -5,10 +5,17 @@ signald() {
 
 if [ -z "$1" ]
   then
-    echo "Please provide the users number as argument in international format"
+    echo "Please provide the users number as argument in international format";
     exit 1;
 fi
 
-signald "{\"account\": \"+4915792453845\", \"address\": { \"number\": \"$1\" }, \"type\": \"get_identities\", \"version\": \"v1\" }" | jq -rc '{"number": .data.address.number, "safety_number": (.data.identities[] | select(.trust_level == "UNTRUSTED") | .safety_number) } | "{\"type\": \"trust\", \"address\": {\"number\": \"\(.number)\"}, \"safety_number\": \"\(.safety_number)\", \"account\": \"+4915792453845\", \"trust_level\": \"TRUSTED_UNVERIFIED\", \"version\": \"v1\"}"' | while read trustcmd; do
-  signald "$trustcmd"
-done
+trustcmd=$(signald "{\"account\": \"+4915792453845\", \"address\": { \"number\": \"$1\" }, \"type\": \"get_identities\", \"version\": \"v1\" }" | jq -rc '{"number": .data.address.number, "safety_number": (.data.identities[] | select(.trust_level == "UNTRUSTED") | .safety_number) } | "{\"type\": \"trust\", \"address\": {\"number\": \"\(.number)\"}, \"safety_number\": \"\(.safety_number)\", \"account\": \"+4915792453845\", \"trust_level\": \"TRUSTED_UNVERIFIED\", \"version\": \"v1\"}"');
+
+if [ -z "$trustcmd" ]
+  then
+    echo "There is no untrusted key for this phone number";
+    exit 1;
+fi
+
+echo "Send: $trustcmd"
+signald "$trustcmd"
